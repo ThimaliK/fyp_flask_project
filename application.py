96ec63759e77016dc8499a2c7e6f83c3e11449f0
ThimaliK <72512995+ThimaliK@users.noreply.github.com>
2023-04-26 01:54:04 +0100

    requirements change 1

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_pymongo import pymongo
+
 
 application = Flask(__name__)
+bcrypt = Bcrypt(application)
+application.secret_key = 'super secret key'
+CONNECTION_STRING = "mongodb+srv://w1761084:6thanos.@cluster0.k2oynue.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('db1')
 
 @application.route('/')
 def hello_world():
     return "MyHealth API"
+
